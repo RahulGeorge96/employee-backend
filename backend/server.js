a4b62054eb286de85e5117a871129cb63dcbfec3
Rahul George <rahuljithu911@gmail.com>
2024-10-28 19:10:35 +0530

    added third

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authRoutes);
-app.use("/api", employeeRoutes);
+app.use("/", authRoutes);
+app.use("/", employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
